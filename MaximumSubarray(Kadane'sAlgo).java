27a3195efdb24a2b7e66a5476098b18707376859
Professor_27 <nanditkalaria27@gmail.com>
2026-06-12 19:15:02 +0530

    q4

---
+++ MaximumSubarray(Kadane'sAlgo).java
@@ -0,0 +1,12 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxi = Integer.MIN_VALUE;w
+        for(int num: nums){
+            sum+=num;
+            maxi = Math.max(maxi,sum);
+            if(sum < 0) sum = 0;
+        }
+        return maxi;
+    }
+}
